80c1a332228c8848f56c7756f6130f5050908eee
adrianasalmon <a2019116341@estudiantes.upsa.edu.bo>
2021-03-20 10:43:49 -0400

    Creacion mvc

---
+++ Producto/Producto/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Producto.Models
+{
+    public class Product
+    {
+        [Key]
+        [Required]
+        [StringLength(99999,ErrorMessage ="The field {0} must contain between {1} and {99999} characters",MinimumLength =1)]
+        public int ProductID { get; set; }
+
+
+        [Required]
+        [StringLength(30, ErrorMessage = "The field {0} must contain between {3} and {30} characters", MinimumLength = 3)]
+        public string Description { get; set; }
+
+       
+        [Required]
+        [DataType(DataType.Currency)]
+        public int Price { get; set; }
+        
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime LastBuy { get; set; }
+
+    }
+}
